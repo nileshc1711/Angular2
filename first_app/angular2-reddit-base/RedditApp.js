9120b7aed6c8a61acc7e925038c2ef348e247a24
Nilesh Chaudhari <nchaudhari@lightbeamhealth.com>
2016-06-28 21:02:13 +0530

    Displaying and adding new articles

--- first_app/angular2-reddit-base/RedditApp.js
+++ first_app/angular2-reddit-base/RedditApp.js
@@ -9,7 +9,7 @@ System.register(['angular2/platform/browser', 'angular2/core'], function(exports
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var browser_1, core_1;
-    var RedditApp;
+    var Article, ArtileComponent, RedditApp;
     return {
         setters:[
             function (browser_1_1) {
@@ -19,21 +19,79 @@ System.register(['angular2/platform/browser', 'angular2/core'], function(exports
                 core_1 = core_1_1;
             }],
         execute: function() {
+            ///Data-Objects -start
+            Article = (function () {
+                function Article(title, link, votes) {
+                    this.votes = votes || 0;
+                    this.title = title;
+                    this.link = link;
+                }
+                Article.prototype.voteUp = function () {
+                    this.votes += 1;
+                };
+                Article.prototype.voteDown = function () {
+                    this.votes -= 1;
+                };
+                return Article;
+            })();
+            ///Data-Objects -end
+            ///Articles Component - start
+            ArtileComponent = (function () {
+                function ArtileComponent() {
+                    // this.article = new Article('Angular 2','http://angular.io',10); // NO Longer needed as the Initailiazation is done 
+                    // in Parent COmponent
+                }
+                ArtileComponent.prototype.voteUp = function () {
+                    this.article.voteUp();
+                    return false;
+                };
+                ArtileComponent.prototype.voteDown = function () {
+                    this.article.voteDown();
+                    return false;
+                };
+                ArtileComponent = __decorate([
+                    core_1.Component({
+                        selector: 'reddit-article',
+                        inputs: ['article'],
+                        host: {
+                            class: 'row'
+                        },
+                        template: "\n    <div class=\"four wide column center aligned votes\">\n    <div class=\"ui statistic\">\n    <div class=\"value\">{{article.votes}}</div>\n    <div class=\"label\"> Points</div>\n    </div>\n    </div>\n    <div class=\"twelve wide column\">\n    <a class=\"ui large header\" href=\"{{article.link}}\">{{article.title}}</a>\n    <ul class=\"ui big horizontal list voters\">\n    <li class=\"item\">\n    <a href (click)=\"voteUp()\">\n    <i class=\"arrow up icon\"></i>\n    upvote\n    </a>\n    </li>\n    <li class=\"item\">\n    <a href (click)=\"voteDown()\">\n    <i class=\"arrow down icon\"></i>\n    downvote\n    </a>\n    </li>\n    </ul>\n    </div>\n\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ArtileComponent);
+                return ArtileComponent;
+            })();
+            ///Articles Component - end
+            ///Main App Component - start
             RedditApp = (function () {
                 function RedditApp() {
+                    this.articles = [
+                        new Article('Angular 2', 'http://angular.io', 3),
+                        new Article('Fullstack', 'http://fullstack.io', 2),
+                        new Article('Angular Homepage', 'http://angular.io', 1)
+                    ];
                 }
+                RedditApp.prototype.addArticle = function (title, link) {
+                    console.log("Adding article title: " + title.value + "  and link : " + link.value);
+                    this.articles.push(new Article(title.value, link.value, 0));
+                    title.value = '';
+                    link.value = '';
+                };
                 RedditApp = __decorate([
-                    // relative import for resolving modules
                     core_1.Component({
                         selector: 'reddit-app',
-                        template: "<form class=\"ui large form segment\">\n <h3 class=\"ui header\">Add a Link</h3>\n\n <div class=\"field\">\n <label for=\"title\">Title:</label>\n <input name=\"title\">\n </div>\n <div class=\"field\">\n <label for=\"link\">Link:</label>\n <input name=\"link\">\n </div>\n </form>\n    "
+                        directives: [ArtileComponent],
+                        template: "<form class=\"ui large form segment\">\n <h3 class=\"ui header\">Add a Link</h3>\n\n <div class=\"field\">\n <label for=\"title\">Title:</label>\n <input name=\"title\" #newtitle> <!--The #newtitle syntax is used to bind input elements to the value variables-->\n </div>\n <div class=\"field\">\n <label for=\"link\">Link:</label>\n <input name=\"link\" #newlink> <!--The #newlink syntax is used to bind input elements to the value variables-->\n </div>\n <button (click)=\"addArticle(newtitle,newlink)\" class=\"ui positive right floated button\" >\n Submit Link\n </button>\n </form>\n<div class=\"ui grid posts\">\n<reddit-article\n*ngFor=\"#article of articles\" [article]=\"article\">\n\n</reddit-article>\n</div>\n\n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], RedditApp);
                 return RedditApp;
             })();
+            ///Main App Component - end
             browser_1.bootstrap(RedditApp);
         }
     }
 });
+//bootstrap(ArtileComponent); 
 //# sourceMappingURL=RedditApp.js.map
--- first_app/angular2-reddit-base/RedditApp.ts
+++ first_app/angular2-reddit-base/RedditApp.ts
@@ -3,20 +3,117 @@
 import { bootstrap } from 'angular2/platform/browser'; // relative import for resolving modules 
 import { Component } from 'angular2/core'; // relative import for resolving modules
 
+///Data-Objects -start
+class Article {
+    votes: number;
+    title: string;
+    link: string;
+    constructor(title: string, link: string, votes?: number) {
+        this.votes = votes || 0;
+        this.title = title;
+        this.link = link;
+
+    }
+
+    voteUp(): void {
+        this.votes += 1;
+    }
+
+
+    voteDown(): void {
+        this.votes -= 1;
+    }
+
+}
+
+
+///Data-Objects -end
+
+
+///Articles Component - start
+@Component({
+    selector: 'reddit-article',
+    inputs: ['article'],
+    host: {
+        class: 'row'
+    },
+    template: `
+    <div class="four wide column center aligned votes">
+    <div class="ui statistic">
+    <div class="value">{{article.votes}}</div>
+    <div class="label"> Points</div>
+    </div>
+    </div>
+    <div class="twelve wide column">
+    <a class="ui large header" href="{{article.link}}">{{article.title}}</a>
+    <ul class="ui big horizontal list voters">
+    <li class="item">
+    <a href (click)="voteUp()">
+    <i class="arrow up icon"></i>
+    upvote
+    </a>
+    </li>
+    <li class="item">
+    <a href (click)="voteDown()">
+    <i class="arrow down icon"></i>
+    downvote
+    </a>
+    </li>
+    </ul>
+    </div>
+
+    `
+
+})
+
+class ArtileComponent {
+    article: Article
+    constructor() {
+        // this.article = new Article('Angular 2','http://angular.io',10); // NO Longer needed as the Initailiazation is done 
+        // in Parent COmponent
+
+    }
+    voteUp(): boolean {
+        this.article.voteUp();
+        return false;
+    }
+    voteDown(): boolean {
+        this.article.voteDown();
+        return false;
+    }
+}
+
+///Articles Component - end
+
+
+
+
+///Main App Component - start
 @Component({
     selector: 'reddit-app',
+    directives: [ArtileComponent],
     template: `<form class="ui large form segment">
  <h3 class="ui header">Add a Link</h3>
 
  <div class="field">
  <label for="title">Title:</label>
- <input name="title">
+ <input name="title" #newtitle> <!--The #newtitle syntax is used to bind input elements to the value variables-->
  </div>
  <div class="field">
  <label for="link">Link:</label>
- <input name="link">
+ <input name="link" #newlink> <!--The #newlink syntax is used to bind input elements to the value variables-->
  </div>
+ <button (click)="addArticle(newtitle,newlink)" class="ui positive right floated button" >
+ Submit Link
+ </button>
  </form>
+<div class="ui grid posts">
+<reddit-article
+*ngFor="#article of articles" [article]="article">
+
+</reddit-article>
+</div>
+
     `
 })
 
@@ -24,8 +121,24 @@ import { Component } from 'angular2/core'; // relative import for resolving modu
  * RedditApp
  */
 class RedditApp {
+
+    articles: Article[];
     constructor() {
-        
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io', 3),
+            new Article('Fullstack', 'http://fullstack.io', 2),
+            new Article('Angular Homepage', 'http://angular.io', 1)
+        ];
+
+    }
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+        console.log(`Adding article title: ${title.value}  and link : ${link.value}`);
+        this.articles.push(new Article(title.value, link.value, 0));
+        title.value = '';
+        link.value = '';
     }
 }
+
+///Main App Component - end
 bootstrap(RedditApp);
+//bootstrap(ArtileComponent);
